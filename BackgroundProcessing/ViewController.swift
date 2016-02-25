0d7c7f87c7b884a2da260dfa5fbd0242d2297273
Brandon Rich <brich@nd.edu1>
2016-02-24 22:19:34 -0500

    initial revision

--- BackgroundProcessing/ViewController.swift
+++ BackgroundProcessing/ViewController.swift
@@ -10,14 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        print("start")
+        
+        var name = "Brandon"
+        self.doInSeconds(5) {
+            (foo) -> Void in
+                print("Hey \(name), I got a value: \(foo)")
+        }
+        
+        // other stuff
+    }
+    
+    func printString( str: String ) -> Void {
+        print("the string is \(str)")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func doInSeconds( seconds: Int, callback: (String) -> Void ) {
+        
+        dispatch_async( dispatch_get_main_queue() ) {
+            // sleep x seconds
+            sleep(UInt32(seconds))
+            // do the callback
+            let msg =  "Processing Finished"
+            callback(msg)
+        }
+        
     }
 
 
